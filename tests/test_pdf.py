c854372351f2cf5a68eb3954e11dc7b75a53fee9
Masetto96 <stefano.scola@protonmail.com>
2023-06-15 20:03:00 +0300

    added tests for pdf func

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,4 +1,5 @@
 import os
+import io
 import pytest
 from pathlib import Path
 
@@ -10,8 +11,11 @@ from requests import HTTPError
 from dotenv import load_dotenv
 
 from oxari.pdf import (
+    _create_key_name,
+    _save_pdf_locally,
     download_pdf,
     _download_template,
+    _fill_pdf,
 )  # import python module
 
 load_dotenv()
@@ -28,9 +32,22 @@ ROOT = Path(__file__).parent.as_posix()
 def test_download_template():
     resp = _download_template(file_name="./template.html", root=ROOT)
     assert type(resp) == Template
+
+def test_create_key_name():
+    resp = _create_key_name(user_id="uuuid8", order_id="1234567890123456789012345678901", space_path=None)
+    assert isinstance(resp, str)
+
+def test_fill_pdf():
+    template_resp = _download_template(file_name="./template.html", root=ROOT)
+    result = _fill_pdf(template_resp, {"user_id" : "uuuid8", "order_id" : "1234567890123456789012345678901"})
+    assert isinstance(result, io.BytesIO)
+
+def test_save_pdf_locally():
+    resp = _save_pdf_locally(binary_data='Hello World'.encode(), user_id = "uuuid8" , order_id = "1234567890123456789012345678901")
+
+    assert resp.get("file") is not None
+
+
+
     
-
-# def test_download_pdf():
-#     resp = download_pdf(user_id="uuuid88", order_id="42", space_path=)
-
 
